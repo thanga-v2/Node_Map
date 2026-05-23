2c17474916046657da44fb866f3e9091ea1f5177
thanga-v2 <thangarajm@icloud.com>
2026-05-23 12:55:37 +0530

    proper write head and statuscode

--- node-crash/server.js
+++ node-crash/server.js
@@ -3,8 +3,19 @@ import http from 'http';
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    res.write('Hello thanga')
-    res.end()
+    // res.write('Hello thanga')
+    // we can change the content type too
+
+
+    // res.setHeader('content-Type' , 'text/html')
+    // res.statusCode=404
+
+    // Instead of above, we can have 'writeHead'
+
+    res.writeHead(200, {'content-type' : 'application/json'} )
+
+
+    res.end(JSON.stringify({message : 'From thanga'}))
 })
 
 server.listen(PORT, () => {
