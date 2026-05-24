7fdc124c74d233f5371390573eeb07036f37eb05
thanga-v2 <thangarajm@icloud.com>
2026-05-24 14:48:45 +0530

    handling file sysytems:

--- node-crash/server.js
+++ node-crash/server.js
@@ -1,10 +1,19 @@
 import { error } from 'console';
 import http from 'http';
 
+import url from 'url';                // ----> to import the file and directory names
+import fs from 'fs/promises';
+import path from 'path';
+
 const PORT = process.env.PORT;
 const API = process.env.API;
 
-const server = http.createServer((req, res) => {
+const __filename = url.fileURLToPath(import.meta.url) // ----> to import the file and directory names
+const __dirname = path.dirname(__filename);
+
+console.log(__dirname, __filename);
+
+const server = http.createServer(async(req, res) => {
     // res.write('Hello thanga')
     // we can change the content type too
 
@@ -21,25 +30,40 @@ const server = http.createServer((req, res) => {
 
         if (req.method === 'GET'){
 
+            let filepath;
+
             // creating a simple router
 
             if(req.url === "/") {
 
-                res.writeHead(200, {'content-type' : 'application/json'} )
-                res.end(JSON.stringify({message : 'From thanga'}))
+                // adding the html file instead
+                // res.writeHead(200, {'content-type' : 'application/json'} )
+                // res.end(JSON.stringify({message : 'From thanga'}))
+
+                filepath = path.join(__dirname, 'public', 'index.html')  // this will go to __dirname/public/index.html
         
             }else if(req.url === "/trustgrid") {
-        
-                res.writeHead(200, {'content-type' : 'application/json'} )
-                res.end(JSON.stringify(
-                    {message : 'For TrustGrid', reponse : "from TrustGrid"},
-                ))
+
+                // adding the html file instead
+
+                // res.writeHead(200, {'content-type' : 'application/json'} )
+                // res.end(JSON.stringify(
+                //     {message : 'For TrustGrid', reponse : "from TrustGrid"},
+                // ))
+
+                filepath = path.join(__dirname, 'public', 'about-trustgrid.html')  // this will go to __dirname/public/about-trustgrid.html
         
             }else {
         
-                res.writeHead(404, {'content-type' : 'text/html'} )
-                res.end('<h1> Not found </h1>')
+                // res.writeHead(404, {'content-type' : 'text/html'} )
+                // res.end('<h1> Not found </h1>')
+
+                throw new Error('Not Found');
             } 
+            const data = await fs.readFile(filepath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end()
         }else {
             throw new Error('Method not allowed');
         }
