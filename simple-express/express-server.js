1d352955b72d702c48ce979486e0c2f1832b2537
thanga-v2 <thangarajm@icloud.com>
2026-05-26 16:57:53 +0530

    reading req.body from routes

--- simple-express/express-server.js
+++ simple-express/express-server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 
 const app = express();  // express is a function, calling it will give app object
+app.use(express.json())    // without this req.body is always undefine
 
 const PORT = process.env.PORT
 
+const users = [
+    {id:1, name:"thanga"},
+    {id:2, name:"mani"},
+    {id:3, name : "kuram"}
+]
+
+// middleware
 const logger = (req, res, next) => {
     console.log(`${req.url} and ${req.method}`)
     next();
 }
 
-// Register middleware globally                                                                                                                                                                                          
+// app.use() -> Register middleware globally                                                                                                                                                                                          
 app.use(logger);     
 
 app.get('/', (req, res) => {
@@ -18,11 +26,23 @@ app.get('/', (req, res) => {
         res.end();
     
     })    
+    
 app.get('/hi', (req, res) => {
     console.log("params",req.params);
     res.send("Hello thanga");
     res.end();
 })    
+
+app.post('/add', (req,res) => {
+    console.log("\n post without app.use(express.json()) \n ",req.body);
+    users.push(req.body);
+    res.send({message : "user added"})
+
+})
+
+app.get('/listusers', (req,res) => {
+    res.send(users);
+})
 
 
 app.listen(PORT, () => {
